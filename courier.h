b7371f97b30eb2bbba84a4706918142fb793bed3
lukovka08 <62939493+lukovka08@users.noreply.github.com>
2020-06-05 16:53:54 +0300

    Add files via upload

---
+++ courier.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+class couries {
+	int name;
+	int id;
+	int phone number;
+	int the time when he will be free;
+	int list of orders;
+public:
+
+};
+
+int main()
+{
+   
+}
---
+++ order.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+class order
+{
+	int id;
+        int price;
+        int delivery time;
+        int delivery address;
+        int id couries;
+public:
+};
+
+int main()
+{
+
+}
