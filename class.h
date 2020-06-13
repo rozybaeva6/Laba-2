3e327a6f0a6bc9a66232287d448555e56fd7e7db
rozybaeva6 <61972638+rozybaeva6@users.noreply.github.com>
2020-06-13 12:18:59 +0300

    Update class.h

--- class.h
+++ class.h
@@ -7,28 +7,32 @@ using namespace std;
 
 class Order {
 public:
-	string ido;
-	int price;
-	int deadline;
-	string address;
-	string courierid;
-	int weight;
-	void addorder(string orderfile);
+	int ido;
+        int price;
+        int deadline;
+        string address;
+        int courierid;
+        int weight;
+	void addorder (string orderfile);
 	void osearchid(string orderfile); // поиск по id
+	void runtime(string orderfile); // время
+	void oselect(string courierfile, string orderfile); // распределение заказов по курьерам
 };
 
 class Courier {
 public:
-	string idc;
+	int idc;	
 	string name;
 	string phone;
-	string orderid1; // текущий заказ
-	string orderid2; // будущий заказ
-	int car;
+	int orderid1; // текущий заказ
+	int orderid2; // будущий заказ
+	int car; 
 	int deliverytime;
-	void addcourier(string courierfile);
+	void addcourier (string courierfile);
 	void courierdeleting(string courierfile); // Удаление по id
-	void csearchname(string courierfile); // поиск по имени
-	void csearchphone(string courierfile); // поиск по телефону
-	void allcourierclear(string courierfile); // Очистить файл с данными
-	};
+	void csearchname(string courierfile); // поиск по имени 
+	void allcourierclear(string courierfile); //Очистить файл с данными
+	void csearchid(string courierfile); // поиск по id
+	void allcourierwrite(string courierfile);
+	void oselect(string courierfile, string orderfile);
+};
