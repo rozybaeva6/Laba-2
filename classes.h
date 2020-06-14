d9ca9afad385b15c71fcae881b2a52d97e891646
rozybaeva6 <61972638+rozybaeva6@users.noreply.github.com>
2020-06-14 20:53:20 +0300

    Update classes.h

--- classes.h
+++ classes.h
@@ -5,42 +5,39 @@
 using namespace std;
 class Order {
 private:
-	int ido;
-        int price;
-        int deadline;
-        string address;
-        int courierid;
-        int dtime;
+	int ido; // id заказа
+        int price; // цена заказа
+        int deadline; // срок доставки заказа
+        string address; // адрес доставки
+        int courierid; // id сотрудника
+        int dtime; // время доставки заказа
 public:
-	friend void allorderclear(string courierfile, string orderfile);
-	friend void allorderwrite(string orderfile);
-	friend void allcourierclear(string courierfile, string orderfile);
-	friend void courierdeleting(string courierfile, string orderfile);
-       friend  void oselect(string courierfile, string orderfile);
-	friend void addorder (string orderfile);
-	friend void osearchid(string orderfile);
-	friend void runtime(string courierfile, string orderfile);
+	friend void allorderclear(string courierfile, string orderfile); // очистить файл заказов
+	friend void allorderwrite(string orderfile); // вывести список заказов
+        friend void oselect(string courierfile, string orderfile); // распределение заказов по курьерам
+	friend void addorder (string orderfile); // добавить заказ
+	friend void osearchid(string orderfile); // поиск по id
+	friend void runtime(string courierfile, string orderfile); // промотать время вперед на n минут
 };
 class Courier {
-private:
-	int idc;	
-	string name;
-	string phone;
-	int orderid1;
-	int orderid2;
-	int car; 
-	int deliverytime;
+private: 
+	int idc; // id сотрудника	
+	string name; // имя сотрудника
+	string phone; // телефон сотрудника
+	int orderid1; // текущий заказ
+	int orderid2; // будущущий заказ
+	int car; // машина для доставки
+	int deliverytime; // время через которое курьер освободится
 public:
-	friend void allorderclear(string courierfile, string orderfile);
-	friend void runtime(string courierfile, string orderfile);
-	friend void csearchid(string courierfile);
-	friend void addcourier(string courierfile);
-	friend void courierdeleting(string courierfile, string orderfile);
-	friend void csearchname(string courierfile);	
-	friend void csearchphone(string courierfile);	
-	friend void allcourierclear(string courierfile, string orderfile);
-	friend void allcourierwrite(string namefile);
-	friend void oselect(string courierfile, string orderfile);
+	friend void runtime(string courierfile, string orderfile); // промотать время вперед на n минут
+	friend void csearchid(string courierfile); // найти курьера по id
+	friend void addcourier(string courierfile); // добавить курьера
+	friend void courierdeleting(string courierfile, string orderfile); // удалить курьера
+	friend void csearchname(string courierfile); // найти курьера по имени
+	friend void csearchphone(string courierfile); // найти курьера по телефону
+	friend void allcourierclear(string courierfile, string orderfile); // очистить список курьеров
+	friend void allcourierwrite(string namefile); // вывести список курьеров
+	friend void oselect(string courierfile, string orderfile); // распределение заказов по курьерам
 };
 void allorderwrite(string orderfile);
 void allorderclear(string courierfile, string orderfile);
@@ -54,4 +51,4 @@ void runtime(string courierfile, string orderfile);
 void csearchid(string courierfile);
 void csearchname(string courierfile);
 void csearchphone(string courierfile);
-void addcourier(string courierfile);
+void addcourier(strng courierfile);
